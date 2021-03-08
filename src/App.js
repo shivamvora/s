2b2e441618183cbb98e6c16110ee64731977153a
shivam vora <59314521+shivamvora@users.noreply.github.com>
2021-03-08 19:03:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import JSONDATA from "./MockData.json";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search...." />
+    </div>
+  );
+}
