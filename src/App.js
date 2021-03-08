7ced494430bde5de9b8e6f11bd20431a56771953
shivam vora <59314521+shivamvora@users.noreply.github.com>
2021-03-08 19:28:55 +0530

    Searching filter done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import JSONDATA from "./MockData.json";
+
 import "./styles.css";
 
 const App = () => {
+  const [searchItem, setSearchItem] = useState("");
   return (
     <div className="App">
-      <input type="text" placeholder="Seaarch..." />
-      {JSONDATA.map((val, key) => {
-        return <div>{val.first_name}</div>;
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchItem(event.target.value);
+        }}
+      />
+      {JSONDATA.filter((val) => {
+        if (searchItem == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchItem.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div key={key}>
+            <p className="design">{val.first_name}</p>
+          </div>
+        );
       })}
     </div>
   );
